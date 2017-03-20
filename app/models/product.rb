44876ea5660019246500e1d7df3275655ca0b5b3
ramfelipe10 <rfelipe@gbox.adnu.edu.ph>
2017-03-20 15:16:09 +0800

    add validator

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ActiveRecord::Base
 IMG_SRC=["bubble","ucream","cushion","cloudcream","eyegel"]
 
- validates :name,:srp, presence: true
-end
+ validates :name,:srp,:retail_price,:distributor_price,:num_stock,:reserved,:image, presence: true
+ validates :name, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
+	validates :srp,:retail_price,:distributor_price,:reserved, numericality: true
+	validates :num_stock,:reserved, :numericality => { :greater_than_or_equal_to => 0}
+ end
