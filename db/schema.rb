c4b26c4d7eb33e53dc88d802e0d61787573e269f
ramfelipe10 <rfelipe@gbox.adnu.edu.ph>
2017-03-20 15:17:46 +0800

    Add status to reservations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316155735) do
+ActiveRecord::Schema.define(version: 20170320055726) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170316155735) do
     t.integer  "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
   create_table "users", force: :cascade do |t|
