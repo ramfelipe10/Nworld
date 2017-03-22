b640cf106a1067c5085e89336d4ecfbe2356cc28
ramfelipe10 <rfelipe@gbox.adnu.edu.ph>
2017-03-22 23:33:52 +0800

    Layout Background, Notice Messages

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -124,6 +124,7 @@ def deletereserve  #For delete Action in the Reservation List
 		
 @reservation=Reservation.find(params[:id])
 @reservation.destroy
+flash[:notice] = "Reservation Successfuly Deleted"
 redirect_to '/reservation'
 	
 end
@@ -144,6 +145,12 @@ def find #SearchBar in Reservation List
 render 'reservations'
 end
 
+def deleteuser #For Deleting a Nworld User
+@user=User.find(params[:id])
+@user.destroy
+flash[:notice] = "User Successfuly Deleted"
+redirect_to '/user'
+end
 
 
 
